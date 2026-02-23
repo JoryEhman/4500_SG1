69daaed8efe5324ca81aabd84ae6a59f340898d3
Jory Ehman <jedhf@umsl.edu>
2026-02-23 12:18:23 -0600

    Initial commit

---
+++ SG1.py
@@ -0,0 +1,10 @@
+def print_Program() -> None:
+    programExplanation = \
+        ("This program is meant to simulate the class Pill Puzzle \n"
+         "You start with N whole pills in a bottle, and each day you the user"
+         "randomly chooses a pill from the assortment of pills.\n ")
+
+    print(programExplanation)
+
+if __name__ == "__main__":
+    print_Program()
