5eec5280db226683abeb26e4a3813e4f1449c761
Jory Ehman <jedhf@umsl.edu>
2026-02-23 20:04:46 -0600

    SG1.py created, program explanation fits Thonny, 1 page

--- SG1.py
+++ SG1.py
@@ -1,10 +1,34 @@
-def print_Program() -> None:
-    programExplanation = \
-        ("This program is meant to simulate the class Pill Puzzle \n"
-         "You start with N whole pills in a bottle, and each day you the user"
-         "randomly chooses a pill from the assortment of pills.\n ")
+"""
+Programming Language: Python 3.8.3
+Development Environment: PyCharm Community Edition (will test in Thonny)
+
+CMP_SCI-4500-001
+Group Members:
+- Jory Ehman
+- Mira Ysabela Ifurung
+- Jacob Schaefer
+- Ray Rulifson
+- Shounak Banerjee
+"""
+
+
+# print_program() prints out the introduction message to users, explaining what will be asked of them, and what
+# they should expect to see as a result.
+def print_program() -> None:
+    programExplanation = (
+        "This program simulates the class Pill Puzzle.\n"
+        "\nYou start with N whole pills in a bottle. Each day, either a (W)hole or (H)alf pill\n"
+        "will be chosen from the assortment of pills being stored in the \"magical pill bottle.\""
+        "\n\t-If you pick a whole pill (W), it must be broken in half, one half\n"
+        "\t is consumed, and the other half is returned to the pill bottle.\n"
+        "\t-If you pick a half pill (H), then you must consume it\n"
+        "\nYou will be prompted to enter:\n"
+        "\tN (1 - 1000): the starting number of whole pills in the bottle\n"
+        "\tR (1 - 10000): the number of simulations that the program will run\n"
+        "\nThis program illustrates interesting statistical phenomenon, by simulating many trials.\n"
+        "")
 
     print(programExplanation)
 
 if __name__ == "__main__":
-    print_Program()
+    print_program()
